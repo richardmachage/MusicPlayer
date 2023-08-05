d4d20fee5a6fc641c9e22e23df77877303866858
richardmachage <rchacha261@gmail.com#>
2023-08-05 16:34:58 +0300

    more on bottom sheets

--- app/src/main/java/com/example/musicplayer/screens/main/BottomSheetViewDetails.kt
+++ app/src/main/java/com/example/musicplayer/screens/main/BottomSheetViewDetails.kt
@@ -1,15 +1,19 @@
 package com.example.musicplayer.screens.main
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.musicplayer.databinding.ViewDetailsBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheetViewDetails : BottomSheetDialogFragment() {
     private  lateinit  var viewDetailsBinding : ViewDetailsBottomSheetBinding
     private val binding get() = viewDetailsBinding
+
+    private val viewModel : MainViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +25,11 @@ class BottomSheetViewDetails : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.inputSongNameEditText.text = Editable.Factory.getInstance().newEditable(viewModel.currentMusic.value?.songName?: "")
+        binding.inputArtistNameEditText.text = Editable.Factory.getInstance().newEditable(viewModel.currentMusic.value?.songArtist?: "")
+        binding.inputAlbumNameEditText.text = Editable.Factory.getInstance().newEditable(viewModel.currentMusic.value?.songAlbum?: "")
+        binding.songPathEditText.text = viewModel.currentMusic.value?.songFilePath?: ""
 
         binding.cancelButton.setOnClickListener {
             this.dismiss()
